83cc00a312f88192803c11c3d5c18772a04cdeb8
KarUladz <u.karnialiuk@gmail.com>
2025-07-02 20:02:58 +0300

    feat: add m3-task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+   void divide(int toBeDivided) {
+        double divideBy = (double) toBeDivided / 5;
+
+        System.out.println(divideBy);
     }
 }
